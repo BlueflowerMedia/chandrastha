35626da64550c422442377f896bc03ec818bfff5
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-20 21:12:04 -0500

    header and footer removed from the page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,5 @@
-<?php get_header(); ?>
+<!-- <?php get_header(); ?> -->
 
 
 <?php echo do_shortcode('[contact-form-7 id="6" title="Contact form 1"]') ?>
-<?php get_footer(); ?>
+<!-- <?php get_footer(); ?> -->
