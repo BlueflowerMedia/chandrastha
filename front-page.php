10e9ba2b395bd9b8385c447e386b5688a6b06e81
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-24 00:32:20 -0500

    google verification added

--- front-page.php
+++ front-page.php
@@ -1,4 +1,16 @@
-<?php get_template_part('template-parts/header-top'); ?>
+<?php get_template_part('template-parts/header-top'); ?><!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+<meta name="google-site-verification" content="LHxAsHwWSmx4s8RoL_6awAYK4yFp0EFDLHcUYfydDqA" />
+  </head>
+  <body>
+
+  </body>
+</html>
+
 
 <?php get_header(); ?>
 
