3ac66098e371ab98a75fc3bbae4d96f0641c6fe1
Chandra Shankar Shrestha <shrestha.zenith@gmail.com>
2018-11-24 00:46:33 -0500

    google verification process

--- front-page.php
+++ front-page.php
@@ -1,10 +1,10 @@
 <?php get_template_part('template-parts/header-top'); ?><!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <meta name="google-site-verification" content="LHxAsHwWSmx4s8RoL_6awAYK4yFp0EFDLHcUYfydDqA" />
     <meta charset="utf-8">
     <title></title>
 
-<meta name="google-site-verification" content="LHxAsHwWSmx4s8RoL_6awAYK4yFp0EFDLHcUYfydDqA" />
   </head>
   <body>
 
